213af2c520383356f2a8f6818ab3cb877fb06468
Franklin <franklin.bdantas@hotmail.com>
2020-05-08 18:53:09 -0300

    IListExtension and unity package architeture finished

--- Assets/Unity Extended/Scripts/Extensions/Vector2Extensions.cs
+++ Assets/Unity Extended/Scripts/Extensions/Vector2Extensions.cs
@@ -6,7 +6,7 @@ namespace UnityExtended.Extensions
     /// <summary>
     /// Class used to create more methos to Vector2 class
     /// </summary>
-    public static class Vector2Extension
+    public static class Vector2Extension//TODO : Vector 3 Extensions
     {
         /// <summary>
         /// Convert the current Vector2 to absolute integer Values
@@ -37,11 +37,28 @@ namespace UnityExtended.Extensions
         /// <returns>Returns true if the x or the y of the first are grat than x and y of second one</returns>
         public static bool AbsGreaterThan(this Vector2 vector2, Vector2 obj) => vector2.ToAbsolute().x > obj.ToAbsolute().x || vector2.ToAbsolute().y > obj.ToAbsolute().y;
     
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vector2"></param>
+        /// <param name="angle"></param>
         public static void FromAngle(this Vector2 vector2,int angle)
         {
             float angleRad = angle * (Mathf.PI / 180f);
-            vector2 = new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+            vector2.x = Mathf.Cos(angleRad);
+            vector2.y = Mathf.Sin(angleRad);
+        }
+
+        /// <summary>
+        /// Return the absolute angle of the target vector2
+        /// </summary>
+        /// <param name="vector2"></param>
+        /// <param name="target"></param>
+        /// <returns>The angle torwards target</returns>
+        public static float GetAngle(this Vector2 vector2,Vector2 target)
+        {
+            var direction = target - vector2;
+            return direction.ToAngle();
         }
 
         public static float ToAngle(this Vector2 vector2)
