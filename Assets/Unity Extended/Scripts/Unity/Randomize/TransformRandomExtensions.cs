6b7f65135eeff8c0de48fa89b350e50e2f244e60
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-07-09 16:20:02 -0300

    Adds TransformRandomExtensions

---
+++ Assets/Unity Extended/Scripts/Unity/Randomize/TransformRandomExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace UnityExtended.Unity.Randomize
+{
+    public static class TransformRandomExtensions
+    {
+        public static void RandomRotation(this Transform transform)
+        {
+            transform.Rotate(
+                xAngle: Random.Range(0, 360),
+                yAngle: Random.Range(0, 360),
+                zAngle: Random.Range(0, 360)
+            );
+        }
+
+        public static void RandomXRotation(this Transform transform)
+        {
+            transform.Rotate(Random.Range(0, 360), 0, 0);
+        }
+
+        public static void RandomYRotation(this Transform transform)
+        {
+            transform.Rotate(0, Random.Range(0, 360), 0);
+        }
+
+        public static void RandomZRotation(this Transform transform)
+        {
+            transform.Rotate(0, 0, Random.Range(0, 360));
+        }
+    }
+}
